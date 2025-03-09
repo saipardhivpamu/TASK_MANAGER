7e20afa84ee22520964d7341ec4d81881759d130
praharshamaroju <marojupraharsha@gmail.com>
2025-03-09 15:38:43 +0530

    mark the completed task feature added

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import json
 import os
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+from tkinter import messagebox
 TASK_FILE = "tasks.json"
 
 def load_tasks():
@@ -11,3 +14,17 @@ def load_tasks():
             return json.load(file)
     except json.JSONDecodeError:
         return []
+def mark_complete():
+
+    selected_item = task_list.selection()
+    if not selected_item:
+        messagebox.showerror("Error", "No task selected!")
+        return
+
+    task_id = task_list.item(selected_item, "values")[0]
+    tasks = load_tasks()
+    for task in tasks:
+        if str(task["id"]) == task_id:
+            task["completed"] = True
+    save_tasks(tasks)
+    update_task_list()
